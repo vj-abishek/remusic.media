74d0331ad2099e8ca7cba2b7fc84a3cf3da117de
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-06-28 21:30:53 +0530

    ADD: manifest, logo, PWA

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useState, useEffect } from 'react';
 import { Router } from '@reach/router';
 import Loader from './Loaders/Spin';
 import './styles/styles.scss';
@@ -16,12 +16,62 @@ const About = lazy(() => import('./components/Profile/Profile'));
 const Details = lazy(() => import('./components/video/Details/Details'));
 
 function App() {
+  const [trigger, setTrigger] = useState(false);
+  const [updateHappen, setUpdateHappen] = useState(false);
+  const [prompt, setPrompt] = useState();
+
+  //listen for update event from the service worker
+  useEffect(() => {
+    const messages = (e) => {
+      console.log(e);
+      console.log('Update happen,testing..');
+      setUpdateHappen(true);
+    };
+
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.ready.then((da) => {
+        da.onupdatefound = messages;
+      });
+    }
+    // navigator.serviceWorker.onmessage = messages
+  }, [updateHappen]);
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      setPrompt(e);
+      // Update UI notify the user they can install the PWA
+      setTrigger(true);
+    });
+  }, [trigger]);
+
+  const HandleClick = () => {
+    // Hide the app provided install promotion
+    setTrigger(false);
+    // Show the install prompt
+    prompt.prompt();
+    // Wait for the user to respond to the prompt
+    prompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
+  };
+
+  const handleCancel = () => setTrigger(false);
+
   return (
     <div style={{ height: '100%', minHeight: '100vh' }}>
       <div className="paddingLeftIS">
         <main className="mainClass">
           <Bottomnnav />
-
+          {trigger && (
+            <Prompt handleClick={HandleClick} Cancel={handleCancel} />
+          )}
           <section>
             <Nav />
             <Suspense fallback={<Loader />}>
